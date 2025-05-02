74224ab97489f0d730edab5c64f93b364a15b3e1
Akshay Nair <akshaynair@Akshays-Laptop.local>
2025-05-02 19:59:17 +0530

    github-pages config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/uticaa-master-master/',
+  base: '/yutika-master-master/',
   plugins: [react()],
 })
